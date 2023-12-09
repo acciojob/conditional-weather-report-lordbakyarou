b11d45faecb36301f0c80b949a6ce79b91e9ba0c
Mayur Hanwate <frostblizard111@gmail.com>
2023-12-09 19:35:51 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,19 @@ import "./../styles/App.css";
 import data from "../data/data";
 
 const App = () => {
-  return (
-    <div>
-      {(data.temperature > 20 && (
-        <p style={{ color: "red" }}>
-          Current Temp : {data.temperature}, Current Conditions:
-          {data.conditions}
-        </p>
-      )) || (
-        <p style={{ color: "blue" }}>
-          Current Temp : {data.temperature}, Current Conditions:
-          {data.conditions}
-        </p>
-      )}
-    </div>
-  );
+  if (data.temperature > 20) {
+    return (
+      <div style={{ color: "red" }}>
+        Current Temp: {data.temperature}, Current condition : {data.conditions}
+      </div>
+    );
+  } else {
+    return (
+      <div style={{ color: "blue" }}>
+        Current Temp: {data.temperature}, Current condition : {data.conditions}
+      </div>
+    );
+  }
 };
 
 export default App;
